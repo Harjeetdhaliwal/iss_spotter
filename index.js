1895b799096a46589d91a1c3d32545fc483b8c57
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-26 19:42:08 -0400

    Created callback hell to fetch the ip , then geo coordinates and then ISS flyover times

---
+++ index.js
@@ -0,0 +1,57 @@
+const {nextISSTimesForMyLocation} = require("./iss");
+
+
+const printPassTimes = function(passTimes){
+  console.log(passTimes);
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+  }
+}
+
+nextISSTimesForMyLocation((error, data) => {
+  if(error){
+    console.log("It didn't work:", error);
+    return;
+  }
+  printPassTimes(data);
+    
+});
+
+
+
+
+
+
+/*
+fetchCoordsByIp("162.245.144.abcdeff", (error, data) => {
+  if(error){
+    console.log("It didn't work:", error);
+    return;
+    }
+    console.log("It worked! Returned: ", data);
+
+});
+
+
+
+fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error,data) => {
+  if (error) {
+    console.log("It didn't work:", error);
+    return;
+  }
+  console.log("It worked! Returned flyover times: ", data);
+})
+
+
+
+
+fetchMyIp((error, ip) => {
+  if(error){
+    console.log("It didn't work:", error);
+    return;
+  }
+  console.log("It worked! Returned IP:", ip);
+});
+*/
