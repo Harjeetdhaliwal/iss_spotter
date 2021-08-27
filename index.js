689a6d6d005cf82694786ee7ed27e8976307c492
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-26 21:24:46 -0400

    Created ISS Spotter with promises

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const {nextISSTimesForMyLocation} = require("./iss");
 
 
 const printPassTimes = function(passTimes){
-  console.log(passTimes);
   for (const pass of passTimes) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
     const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
 }
 
